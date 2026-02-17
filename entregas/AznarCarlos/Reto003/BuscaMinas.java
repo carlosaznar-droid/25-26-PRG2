d78e5db0fb2380203992e4832573cfee3c0095b0
CarlosAznar <carlos.aznar@alumnos.uneatlantico.es>
2026-02-17 12:54:33 +0100

    creamos nuestras primeras matrices/Variables para tener una base con la que trabajar

---
+++ entregas/AznarCarlos/Reto003/BuscaMinas.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BuscaMinas {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String[][] tablero = new String[5][7];
+		String[][] visible = new String[5][7];
+
+		boolean salida = false;
+
+	}
+}
