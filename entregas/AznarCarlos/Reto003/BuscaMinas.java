d6cfab19a475d41b6e6c565f7c8e82695b195afb
CarlosAznar <carlos.aznar@alumnos.uneatlantico.es>
2026-02-17 13:10:25 +0100

    Se sigue con el metodo para crear el tablero y creamos la parte de arriba

--- entregas/AznarCarlos/Reto003/BuscaMinas.java
+++ entregas/AznarCarlos/Reto003/BuscaMinas.java
@@ -2,46 +2,56 @@ import java.util.Scanner;
 
 public class BuscaMinas {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-		String[][] tablero = new String[5][7];
-		String[][] visible = new String[5][7];
+        String[][] tablero = new String[5][7];
+        String[][] visible = new String[5][7];
 
-		boolean salida = false;
+        boolean salida = false;
 
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 7; j++) {
-				tablero[i][j] = "-";
-				visible[i][j] = "-";
-			}
-		}
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                tablero[i][j] = "-";
+                visible[i][j] = "-";
+            }
+        }
 
-		tablero[0][4] = "*";
+       
+        tablero[0][4] = "*";
 
-		while (!salida) {
+        while (!salida) {
 
-			imprimirTablero(visible);
+            imprimirTablero(visible);
 
-			System.out.println("Ingrese X");
-			int x = sc.nextInt();
+            System.out.println("Ingrese X");
+            int x = sc.nextInt();
 
-			System.out.println("Ingrese Y");
-			int y = sc.nextInt();
+            System.out.println("Ingrese Y");
+            int y = sc.nextInt();
 
-			System.out.println("----------------");
+            System.out.println("----------------");
 
-			if (tablero[x - 1][y - 1] == "*") {
-				visible[x - 1][y - 1] = "*";
-				System.out.println("Mala suerte has encontrado una mina");
-				salida = true;
-			} else {
-				visible[x - 1][y - 1] = ".";
-				System.out.println("Esta Libre no hay mina");
-			}
-		}
+            if (tablero[x - 1][y - 1] == "*") {   
+                visible[x - 1][y - 1] = "*";
+                System.out.println("Mina!");
+                salida = true;
+            } else {
+                visible[x - 1][y - 1] = ".";
+                System.out.println("Libre!");
+            }
+        }
 
-		sc.close();
+        imprimirTablero(visible);
+
+        sc.close();
+    }
+
+    public static void imprimirTablero(String[][] tablero) {
+
+        System.out.println("================");
+        System.out.print("  ");
+
 	}
 }
