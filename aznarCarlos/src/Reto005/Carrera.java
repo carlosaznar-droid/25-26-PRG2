12d83257003af1842ebb6a9e3a588883a243d846
Cacarlitros <carlos.aznar@alumnos.uneatlantico.es>
2026-04-27 20:28:50 +0200

    Añadimos avanzarCaballos

---
+++ aznarCarlos/src/Reto005/Carrera.java
@@ -0,0 +1,33 @@
+class Carrera {
+     public static void main(String[] args) {
+       
+    }
+
+    
+    private int caballo1 = 0;
+    private int caballo2 = 0;
+    private int meta = 40;
+
+     public void jugar() {
+        int turno = 1;
+
+        while (caballo1 < meta && caballo2 < meta) {
+            System.out.println("TURNO NÚMERO: " + turno);
+
+            avanzarCaballos();
+            mostrarPista();
+
+            turno++;
+            pausa(); 
+        }
+
+        mostrarResultado();
+    }
+    
+     private void avanzarCaballos() {
+        caballo1 += (int) (Math.random() * 3); 
+        caballo2 += (int) (Math.random() * 3);
+    }
+    
+    
+}
