f687b90a12e320f20b6183b7df1a41473b1845bd
Cacarlitros <carlos.aznar@alumnos.uneatlantico.es>
2026-05-12 20:47:41 +0200

    Juega el crupier

--- entregas/aznarCarlos/src/Reto-006/Dealer.java
+++ entregas/aznarCarlos/src/Reto-006/Dealer.java
@@ -1,3 +1,25 @@
 public class Dealer{
-    
+    private Mano mano = new Mano();
+    public boolean tieneBlackJack(){
+        if(mano.getValor() == 21){
+            return true;
+        
+    }else{
+        return false;
+    }
+ }
+    public void  jugarCrupier(Mazo baraja){
+
+        System.out.println();
+
+        while(mano.getValor() < 17){
+            mano.agregarCarta(baraja.repartirCarta());
+        }
+    }
+    if (mano.getValor() > 21){
+        System.out.println("El crupier se ha pasado de 21, el jugador gana.");
+
+}else{
+        System.out.println("El crupier tiene un valor de " + mano.getValor() + ", el jugador pierde.");
+    }
 }
