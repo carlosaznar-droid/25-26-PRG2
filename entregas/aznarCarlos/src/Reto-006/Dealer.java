a9a794686ef7bff5fde271500814452c1d183095
Cacarlitros <carlos.aznar@alumnos.uneatlantico.es>
2026-05-12 20:49:35 +0200

    Añadir carta

--- entregas/aznarCarlos/src/Reto-006/Dealer.java
+++ entregas/aznarCarlos/src/Reto-006/Dealer.java
@@ -15,11 +15,16 @@ public class Dealer{
         while(mano.getValor() < 17){
             mano.agregarCarta(baraja.repartirCarta());
         }
-    }
+    
     if (mano.getValor() > 21){
         System.out.println("El crupier se ha pasado de 21, el jugador gana.");
 
 }else{
         System.out.println("El crupier tiene un valor de " + mano.getValor() + ", el jugador pierde.");
     }
+
+}
+public void agregarCarta(Carta carta){
+    mano.agregarCarta(carta);
+}
 }
