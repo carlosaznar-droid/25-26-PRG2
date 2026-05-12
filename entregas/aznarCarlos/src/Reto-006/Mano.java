ed824576d60f66222bec96b60f9470aecd2efb9f
Cacarlitros <carlos.aznar@alumnos.uneatlantico.es>
2026-05-12 21:26:35 +0200

    esCrupier y mostrar Carta oculta ademas de si es as sumar 1 o 11

--- entregas/aznarCarlos/src/Reto-006/Mano.java
+++ entregas/aznarCarlos/src/Reto-006/Mano.java
@@ -15,4 +15,44 @@ int total = 0;
         }
         return total;
     }
+    public String toString(){
+        return this.toString(false,false);
     }
+    public String toString(boolean esCrupier,boolean ocultarCartaOculta){
+        String texto = "";
+        int total = 0;
+        boolean hayAs = false;
+        string textoAs = "";
+        for(int i=0;i<numCartas;i++){
+            if(esCrupier && ocultarCartaOculta && i==0){
+                texto = "mostrando";
+            }else{
+                int valor = mano[i].getValor();
+                String nombreValor;
+                if (valor >10){
+                    nombreValor = mano[i].getNombreValor().substring(0,1);
+                }else if (valor == 1){
+                    nombreValor = "A";
+            }else{
+                nombreValor = Integer.toString(valor);
+            }
+        texto += nombreValor + " de " + mano[i].getPalo() + "\n";
+        if (valor > 10){
+            valor += 10;
+    }else if (valor == 1){
+        hayAs = true;
+       
+    }
+        total += valor;
+    }
+        }
+        if (hayA && total +10<=21){
+            textoAS= "o"+ (total+10);
+        }
+        if (ocultarCartaOculta){
+           retiurn texto;
+        }else{
+            return texto + "Total: " + total + textoAs;
+        }
+    }
+    
