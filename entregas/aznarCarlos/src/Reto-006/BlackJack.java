644f7356626406946a73daeb4cbeec07479df13a
Cacarlitros <carlos.aznar@alumnos.uneatlantico.es>
2026-05-12 18:35:44 +0200

    Empezamos la clase BlackJack

---
+++ entregas/aznarCarlos/src/Reto-006/BlackJack.java
@@ -0,0 +1,17 @@
+class BlackJack {
+    public static void main(String[] args) {
+
+        private int jugador;
+        private int dealer;
+        private int banca = 21;
+        private int resultado;
+        
+        public BlackJack(int jugador, int dealer) {
+
+            this.jugador = jugador;
+            this.dealer = dealer;
+
+        }
+        
+      
+}
