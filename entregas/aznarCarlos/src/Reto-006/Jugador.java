ae288805aeca6263981d200f05711e99db6087e5
Cacarlitros <carlos.aznar@alumnos.uneatlantico.es>
2026-05-12 21:32:53 +0200

    objeto Jugador

--- entregas/aznarCarlos/src/Reto-006/Jugador.java
+++ entregas/aznarCarlos/src/Reto-006/Jugador.java
@@ -1,3 +1,10 @@
 public class Jugador{
-    
+    private int banco;
+    private int apuesta;
+    private String nombre;
+    private Mano mano;
+    public Jugador(){
+        banco = 100;
+        mano = new Mano();
+    }
 }
